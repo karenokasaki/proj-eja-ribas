922f98a2246e210b82a297b74ca4a836a976397a
karen okasaki <oks.karen@gmail.com>
2023-03-16 03:47:13 -0300

    login and signup page done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom";
-import { Fragment, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Fragment, useRef, useState } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import eja from "../../assets/eja.jpeg";
 import jopa from "../../assets/jopa.jpeg";
+import { api } from "../../api/api";
 
 export function Home() {
   const coordRef = useRef(null);
   const signupRef = useRef(null);
+  const navigate = useNavigate();
 
-  const scrollToRef = (ref) => {
-    if (ref.current) {
-      ref.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    phone: "",
+  });
 
   const navigation = [{ name: "Cadastre-se" }, { name: "Coordenação" }];
   const collabs = [
@@ -71,6 +75,28 @@ export function Home() {
       },
     ],
   };
+
+  const scrollToRef = (ref) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await api.post("/users/sign-up", { ...form });
+
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log(form);
   return (
     <div className="bg-white">
       <header className="relative bg-sky-800 pb-24 sm:pb-32">
@@ -92,7 +118,7 @@ export function Home() {
           >
             <div className="flex w-full items-center justify-between lg:w-auto">
               <a href="#">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">Eja</span>
                 <img className="h-20 w-auto sm:h-24" src={eja} alt="eja" />
               </a>
               <div className="-mr-2 flex items-center lg:hidden">
@@ -146,7 +172,7 @@ export function Home() {
                   </div>
                   <div className="-mr-2">
                     <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-stone-400 hover:bg-stone-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
-                      <span className="sr-only">Close menu</span>
+                      <span className="sr-only">Fechar menu</span>
                       <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                     </Popover.Button>
                   </div>
@@ -154,13 +180,17 @@ export function Home() {
                 <div className="pt-5 pb-6">
                   <div className="space-y-1 px-2">
                     {navigation.map((item) => (
-                      <a
+                      <button
                         key={item.name}
-                        href={item.href}
+                        onClick={() => {
+                          if (item.name === "Coordenação")
+                            scrollToRef(coordRef);
+                          else scrollToRef(signupRef);
+                        }}
                         className="block rounded-md px-3 py-2 text-base font-medium text-stone-900 hover:bg-stone-50"
                       >
                         {item.name}
-                      </a>
+                      </button>
                     ))}
                   </div>
                   <div className="mt-6 px-5">
@@ -192,7 +222,7 @@ export function Home() {
       <main>
         {/* Side-by-side grid */}
         <div className="bg-white" ref={signupRef}>
-          <div className="mx-auto max-w-md py-24 px-6 sm:max-w-3xl sm:py-8 lg:max-w-7xl lg:px-8">
+          <div className="mx-auto max-w-md py-18 px-6 sm:max-w-3xl sm:py-8 lg:max-w-7xl lg:px-8">
             <div className="divide-y divide-stone-200">
               <section aria-labelledby="contact-heading">
                 <h2
@@ -216,13 +246,15 @@ export function Home() {
                         htmlFor="first-name"
                         className="block text-sm font-medium leading-6 text-gray-900"
                       >
-                        First name
+                        Nome completo
                       </label>
                       <div className="mt-2">
                         <input
+                          id="formName"
+                          name="name"
                           type="text"
-                          name="first-name"
-                          id="first-name"
+                          value={form.name}
+                          onChange={handleChange}
                           autoComplete="given-name"
                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
@@ -234,15 +266,17 @@ export function Home() {
                         htmlFor="last-name"
                         className="block text-sm font-medium leading-6 text-gray-900"
                       >
-                        Last name
+                        Telefone
                       </label>
                       <div className="mt-2">
                         <input
-                          type="text"
-                          name="last-name"
-                          id="last-name"
-                          autoComplete="family-name"
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          type="phone"
+                          name="phone"
+                          id="phone"
+                          value={form.phone}
+                          onChange={handleChange}
+                          autoComplete="phone"
+                          className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                       </div>
                     </div>
@@ -252,53 +286,16 @@ export function Home() {
                         htmlFor="email"
                         className="block text-sm font-medium leading-6 text-gray-900"
                       >
-                        Email address
+                        Email
                       </label>
                       <div className="mt-2">
                         <input
-                          id="email"
+                          id="formEmail"
                           name="email"
                           type="email"
+                          value={form.email}
+                          onChange={handleChange}
                           autoComplete="email"
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="sm:col-span-3">
-                      <label
-                        htmlFor="country"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Country
-                      </label>
-                      <div className="mt-2">
-                        <select
-                          id="country"
-                          name="country"
-                          autoComplete="country-name"
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        >
-                          <option>United States</option>
-                          <option>Canada</option>
-                          <option>Mexico</option>
-                        </select>
-                      </div>
-                    </div>
-
-                    <div className="sm:col-span-6">
-                      <label
-                        htmlFor="street-address"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        Street address
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          type="text"
-                          name="street-address"
-                          id="street-address"
-                          autoComplete="street-address"
                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                       </div>
@@ -309,63 +306,36 @@ export function Home() {
                         htmlFor="city"
                         className="block text-sm font-medium leading-6 text-gray-900"
                       >
-                        City
+                        Senha
                       </label>
                       <div className="mt-2">
                         <input
-                          type="text"
-                          name="city"
-                          id="city"
-                          autoComplete="address-level2"
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="region"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        State / Province
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          type="text"
-                          name="region"
-                          id="region"
-                          autoComplete="address-level1"
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="sm:col-span-2">
-                      <label
-                        htmlFor="postal-code"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        ZIP / Postal code
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          type="text"
-                          name="postal-code"
-                          id="postal-code"
-                          autoComplete="postal-code"
+                          id="formPassword"
+                          name="password"
+                          type="password"
+                          value={form.password}
+                          onChange={handleChange}
+                          autoComplete="password"
                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
                       </div>
                     </div>
                   </div>
                 </div>
+                <button
+                  type="submit"
+                  onClick={handleSubmit}
+                  className="mt-12 rounded-md border border-transparent bg-green-700 bg-opacity-2 py-2 px-6 text-base font-medium text-white hover:bg-opacity-80"
+                >
+                  Cadastrar
+                </button>
               </section>
             </div>
           </div>
         </div>
 
         {/* Newsletter */}
-        <div className="relative pt-12">
+        <div className="relative pt-4">
           <div
             className="absolute left-0 right-0 h-1/2 bg-stone-50"
             aria-hidden="true"
@@ -380,7 +350,7 @@ export function Home() {
                   Um pouco da história da escola.
                 </p>
               </div>
-              <div className="mt-8 flex gap-5 sm:w-full sm:max-w-md lg:mt-0 lg:ml-8 lg:flex-1">
+              <div className="mt-8 flex flex-wrap justify-center gap-5 sm:w-full sm:flex-row lg:mt-0 lg:ml-8 lg:flex-1">
                 <img
                   src={jopa}
                   alt="logo da escola"
@@ -398,7 +368,7 @@ export function Home() {
 
         {/* FAQ */}
         <div className="bg-stone-50" ref={coordRef}>
-          <div className="mx-auto max-w-md py-6 px-6 sm:max-w-3xl sm:py-24 lg:max-w-7xl lg:px-8">
+          <div className="mx-auto max-w-md py-6 px-6 sm:max-w-3xl sm:py-12 lg:max-w-7xl lg:px-8">
             <div className="lg:grid lg:grid-cols-3 lg:gap-8">
               <div>
                 <h2 className="text-3xl font-bold tracking-tight text-stone-900">
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,8 @@ import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../../api/api";
 import { AuthContext } from "../../contexts/authContext";
+import eja from "../../assets/eja.jpeg";
+import jopa from "../../assets/jopa.jpeg";
 
 export function Login() {
   const [form, setForm] = useState({
@@ -33,31 +35,81 @@ export function Login() {
   }
 
   return (
-    <form onSubmit={handleSumit}>
-      <label>Email:</label>
-      <input
-        type="email"
-        name="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      <label>Senha:</label>
-      <input
-        type="password"
-        name="password"
-        value={form.password}
-        onChange={handleChange}
-      />
+    <>
+      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Acesse sua conta
+          </h2>
+        </div>
 
-      <button
-        type="submit"
-        className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold
-      text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
-      focus-visible:outline-2 focus-visible:outline-offset-2
-      focus-visible:outline-indigo-600"
-      >
-        Entrar!
-      </button>
-    </form>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form className="space-y-6" onSubmit={handleSumit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Senha
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="password"
+                    name="password"
+                    value={form.password}
+                    onChange={handleChange}
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Entrar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        <img
+          className="mx-auto h-28 w-auto rounded-full md:h-36"
+          src={eja}
+          alt="Your Company"
+        />
+        <img
+          className="mx-auto h-28 w-auto rounded-full md:h-36"
+          src={jopa}
+          alt="Your Company"
+        />
+      </div>
+    </>
   );
 }
